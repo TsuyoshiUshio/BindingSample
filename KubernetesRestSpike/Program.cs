6eefa1e0bb806afd2165a38fabc6b316222563b9
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-02-25 20:12:14 +0900

    Adding sample of the settings

---
+++ KubernetesRestSpike/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KubernetesRestSpike
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static Program()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPlicyErrors) => true;
+            client = new HttpClient(httpClientHandler);            
+        }
+
+        static HttpClient client;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("E:" + System.Environment.GetEnvironmentVariable("serverUrl"));
+            Console.WriteLine("C:" + Configuration["serverUrl"]);
+
+            RunAsync().GetAwaiter().GetResult();
+
+            Console.ReadLine();
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri(Configuration["serverUrl"]);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Configuration["kubernetesToken"]);
+            var response = await client.GetAsync("/api/v1/namespaces/default/pods");
+            var result = await response.Content.ReadAsStringAsync();
+            var resultOject = JsonConvert.DeserializeObject<Rootobject>(result);
+            
+            foreach(var item in resultOject.items)
+            {
+
+                Console.WriteLine($"Pod: {item.metadata.name}");
+                Console.WriteLine($"Status: {item.status.phase}");
+                var ts = DateTime.UtcNow - item.status.startTime;
+                Console.WriteLine($"Started {ts.TotalMinutes} min before");
+            }
+
+          //  Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -4,6 +4,10 @@ For my learning, I implement the Kubernetes bindings for Azure Functions.
 
 # Configuration
 
+## JobHostSample
+
+JobHostSample is WebJobs local environment.
+
 Set the Environemnt Variables on your Visual Studio. Then start Storage Emulator on your Windows PC. 
 
 | Name | Value |
@@ -13,6 +17,25 @@ Set the Environemnt Variables on your Visual Studio. Then start Storage Emulator
 |AzureWebJobsDashboard|UseDevelopmentStorage=true|
 
 Hit the JobSample then it start WebJobs. 
+
+## KubernetesRestSpike
+
+Spike solution for Kubernetes Trigger. You need to create appsettings.json. Copy the appsettings.json_example to the appsetting.json. Then put it on your secret information from your Kubernetes cluster.
+
+_SERVER_URL_
+
+```
+kubectl config view | grep server | cut -f 2- -d ":" | tr -d " "
+```
+
+_KUBERNETES_TOKEN_
+
+```
+kubectl describe secret $(kubectl get secrets | \
+  grep default | cut -f1 -d ' ') | \
+  grep -E '^token' | cut -f2 -d':' | tr -d '\t')
+```
+
 
 # Note
 
